5295aff47b1a08f37aaa1642c299787f4282b11e
cesarSMOB <cesar@smob.io>
2026-01-21 23:04:14 -0600

    estilos en CardRestaurant

--- src/feactures/restaurants/components/CardRestaurant.jsx
+++ src/feactures/restaurants/components/CardRestaurant.jsx
@@ -5,25 +5,27 @@ import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 export function CardRestaurant({name,address,phone,site,rating}) {
   return (
     <Card sx={{ width: 275, p: 2 }}>
-      <Typography variant="subtitle1">{name}</Typography>
+      <Typography variant="subtitle1">
+        <strong>{name}</strong>
+      </Typography>
       <Rating value={rating} readOnly></Rating>
       <Divider sx={{my:2}}></Divider>
       <Stack spacing={1}>
         <Stack direction="row" spacing={1} alignItems="flex-start">
-          <PlaceOutlinedIcon size={16} color="disabled" />
-          <Typography variant="body2" color="textSecondary">
+          <PlaceOutlinedIcon size={16} color="info" />
+          <Typography variant="body2">
             {address}
           </Typography>
         </Stack>
         <Stack direction="row" spacing={1} alignItems="center">
-          <LocalPhoneOutlinedIcon size={16} color="disabled" />
-          <Typography variant="body2" color="textSecondary">
+          <LocalPhoneOutlinedIcon size={16} color="info" />
+          <Typography variant="body2">
             {phone}
           </Typography>
         </Stack>
         <Stack direction="row" spacing={1} alignItems="center">
-          <LanguageOutlinedIcon size={16} color="disabled" />
-          <Link href={""} target="_blank" variant="body2" color="textSecondary">
+          <LanguageOutlinedIcon size={16} color="info" />
+          <Link href={""} target="_blank" variant="body2">
             {site}
           </Link>
         </Stack>
